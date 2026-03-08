76f6ab0173c7ae8b3eacee0d10616f2fe2f32e2b
Poly_Xalxo <denilbinodxalxo12@gmail.com>
2026-03-08 12:37:10 +0530

    Update clustering.py

--- src/clustering.py
+++ src/clustering.py
@@ -1,11 +1,20 @@
 import folium
+from sklearn.cluster import KMeans
+
+coords = df[['Lat','Lon']]
+
+kmeans = KMeans(n_clusters=5)
+df['cluster'] = kmeans.fit_predict(coords)
 
 map = folium.Map(location=[40.75, -73.98], zoom_start=12)
 
-for i in range(1000):
+colors = ['red','blue','green','purple','orange']
+
+for i in range(min(1000, len(df))):
     folium.CircleMarker(
         location=[df.iloc[i]['Lat'], df.iloc[i]['Lon']],
-        radius=2
+        radius=2,
+        color=colors[df.iloc[i]['cluster']]
     ).add_to(map)
 
 map
